0a9e6e5a258a296d157d946ea256e8a706157836
yang.wu <yang.wu@ctrip.com>
2017-03-27 14:14:53 +0800

    add one vuex demo

---
+++ grammer-exercise/demo29/src/components/Hello.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <p>{{count}}</p>
+  </div>
+</template>
+
+<script>
+import store from '../store.js'
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  computed:{
+    count(){
+      return store.state.count
+    }
+  },
+  methods:{
+    increment(){
+      store.commit('increment')
+    },
+    decrement(){
+      store.commit('decrement')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
